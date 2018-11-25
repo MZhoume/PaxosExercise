aa402b26e409c39f32aa0bd1ffc6363797c4e1ae
M Zhou <contact@mzhou.me>
2018-11-25 18:58:49 -0500

    Automatically redirect user from home to swagger ui

---
+++ Challenge1/Features/HomeController.cs
@@ -0,0 +1,24 @@
+using System;
+using Challenge1.Core.Swagger;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Challenge1.Features
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private readonly SwaggerOptions _swaggerOptions;
+
+        public HomeController(IOptions<SwaggerOptions> options)
+        {
+            this._swaggerOptions = options.Value ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return this.Redirect(this._swaggerOptions.Endpoint);
+        }
+    }
+}
